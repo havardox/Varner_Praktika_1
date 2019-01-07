15aa1dd7b416d9bc8be160755d1f2e50830160a2
Harald Varner <harald.varner@gmail.com>
2019-01-07 14:09:36 +0200

    Yes

--- Varner/Program.cs
+++ Varner/Program.cs
@@ -18,7 +18,7 @@ namespace Varner
                 Console.WriteLine("3 - Ülesanne 3");
                 char c = Console.ReadKey().KeyChar;
                 if (c == '1') YL01();
-                //else if (c == '2') YL02();
+                else if (c == '2') YL02();
                 //else if (c == '3') YL03();
                 else break;
             } while (true);
@@ -28,17 +28,88 @@ namespace Varner
         {
             Console.Write("\n\nKorrutustabel (15x15) \n\n");
 
-            int[,] tab = new int[15, 15];
+            int[,] tab = new int[16, 16];
 
-            for (int i = 1; i <= 15; i++)
+            for (int i = 0; i < 16; i++)
             {
-                for (int j = 1; j <= 15; j++)
+                for (int j = 0; j < 16; j++)
                 {
-                    tab[i - 1, j - 1] = i * j;
-                    Console.Write(tab[i - 1, j - 1] + "\t");
+                    tab[i, j] = i * j;
+                    Console.Write("{0,4}",tab[i, j]);
                 }
                 Console.WriteLine();
             }
+        }
+
+        static void YL02()
+        {
+            Console.Write("\n\nÜlesanne 2 \n\n");
+            // Formeerida kahemõõtmeline massiiv 10x10
+            // juhuarvudest vahemikust [10,99]
+            // Diagonaalid on 0
+            // ja leida selle massiivi maksimaalsete arvude asukohad.
+            Random rand = new Random();
+            int[,] tab = new int[10,10];
+            int i, j;
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 10; j++)
+                {
+                    tab[i, j] = rand.Next(10, 100);
+                }
+                tab[i, i] = 0;
+                tab[i, tab.GetLength(1) - 1 - i] = 0;
+            }
+
+            PrintMatrix(tab);
+
+            int max = tab[0, 0];
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 10; j++)
+                {
+                    if (tab[i, j] > max)
+                    {
+                        max = tab[i, j];
+                    }
+                }
+            }
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 10; j++)
+                {
+                    Console.Write("{0,4}", tab[i, j]);
+                }
+                Console.WriteLine("");
+                for (j = 0; j < 10; j++)
+                {
+                    if (tab[i,j] == max)
+                    {
+                        Console.Write("  --");
+                    }
+                    else
+                    {
+                        Console.Write("    ");
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static void PrintMatrix(int[,] arr)
+        {
+            int numLines = arr.GetLength(0);
+            int numCols = arr.GetLength(1);
+            Console.Write("Matrix {0}x{1} väljatrükk\n\n", numLines, numCols);
+            for (int i = 0; i < numLines; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    Console.Write("{0,4}", arr[i, j]);
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
         }
     }
 }
